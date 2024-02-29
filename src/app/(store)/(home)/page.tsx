e5e591f38c9a565c012364930fda2161a85f7274
Lucas Andrade <contato.developerlucas@gmail.com>
2024-02-29 19:56:33 -0300

    feat: search-form api, make the search form in header works

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,14 +1,23 @@
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const respose = await api('/products/featured')
+  const respose = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60,
+    },
+  })
 
   const products = await respose.json()
 
   return products
+}
+
+export const metadata: Metadata = {
+  title: 'Home',
 }
 
 export default async function Home() {
